e5399b1f9cddea0fd9a5d701e970c620e7a79897
Truls H <fanthoft@gmail.com>
2020-06-03 11:32:25 +0200

    fix formatting for help function

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const { prefix } = require('../config.json');
 
 module.exports = {
 	name: 'help',
-	description: 'List all of my commands or info about a specific command.',
+	description: 'List all of my commands or get info about a specific command.',
 	aliases: ['commands'],
 	usage: '[command name]',
 	cooldown: 10,
@@ -13,11 +13,12 @@ module.exports = {
         // Did they ask for help with a specific command?
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join('\n'));
+            data.push(commands.map(command => `${prefix}${command.name}  \`${command.description}\``).join('\n'));
             data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
             return message.reply(data, { split: true })
         }
+
         const name = args[0].toLowerCase();
         const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
@@ -29,7 +30,7 @@ module.exports = {
 
         if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
         if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        if (command.usage) data.push(`**Usage:** \`${prefix}${command.name} ${command.usage}\``);
 
         data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
