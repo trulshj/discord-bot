1a9e4f7546540bec7fa453414946b5885b5f7269
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2020-10-26 22:19:59 +0100

    Update grillkongen command

--- commands/grillkongen.js
+++ commands/grillkongen.js
@@ -1,9 +1,12 @@
 module.exports = {
-    name: 'grillkonge',
-    aliases: ['gk', 'grillkongen'],
-    description: 'Poster et bilde av selveste grillkongen',
-    execute(message, args) {
-        const food = ['pølser', 'hamburger', 'ostepølse', 'stek'];
-        message.channel.send(`Nam nam, jeg lukter ${food[Math.floor((Math.random()*food.length))]}!`, {files: ['./images/grillkongen.PNG']});
-    },
+  name: "grillkonge",
+  aliases: ["gk", "grillkongen"],
+  description: "Poster et bilde av selveste grillkongen",
+  execute(message, args) {
+    const food = ["pølser", "hamburger", "ostepølse", "stek"];
+    let randomFood = food[Math.floor(Math.random() * food.length)];
+    message.channel.send(`Nam nam, jeg lukter ${randomFood}!`, {
+      files: ["./images/grillkongen.png"],
+    });
+  },
 };
